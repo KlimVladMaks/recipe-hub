90b0a8fda91cf640c4967dcf773692fe6fe3e79a
vlad <vkm.blog@yandex.ru>
2026-04-17 18:12:21 +0300

    Работа над API (6)

--- src/controllers/directory.controller.ts
+++ src/controllers/directory.controller.ts
@@ -5,8 +5,13 @@ import {
     DishTypeReadListSchema, 
     DishTypeReadSchema, 
     DishTypeUpdateSchema, 
+    IngredientReadListSchema, 
+    IngredientReadSchema, 
+    IngredientUpdateSchema, 
     type DishTypeCreateType, 
-    type DishTypeUpdateType 
+    type DishTypeUpdateType, 
+    type IngredientCreateType,
+    type IngredientUpdateType
 } from "../schemas/directory.schema.js";
 import { DirectoryService } from "../services/directory.service.js";
 
@@ -105,6 +110,15 @@ export class DirectoryController {
 
     static async getIngredients(req: AuthRequest, res: Response) {
         try {
+            const { 
+                page:pageStr='1', 
+                limit:limitStr='20',
+                search='',
+            } = req.query;
+            const page = parseInt(pageStr as string);
+            const limit = parseInt(limitStr as string);
+            const ingredients = await DirectoryService.getIngredients(search as string, page, limit);
+            res.status(200).json(IngredientReadListSchema.parse(ingredients));
         } catch (error: any) {
             res.status(400).json({
                 message: error.message,
@@ -121,6 +135,9 @@ export class DirectoryController {
                 })
                 return;
             };
+            const ingredientCreateData: IngredientCreateType = req.body;
+            const ingredient = await DirectoryService.createIngredient(ingredientCreateData);
+            res.status(201).json(IngredientReadSchema.parse(ingredient));
         } catch (error: any) {
             res.status(400).json({
                 message: error.message,
@@ -130,6 +147,10 @@ export class DirectoryController {
 
     static async getIngredient(req: AuthRequest, res: Response) {
         try {
+            const { ingredientId:ingredientIdStr } = req.params;
+            const ingredientId = parseInt(ingredientIdStr as string);
+            const ingredient = await DirectoryService.getIngredient(ingredientId);
+            res.status(200).json(IngredientReadSchema.parse(ingredient))
         } catch (error: any) {
             res.status(400).json({
                 message: error.message,
@@ -146,6 +167,11 @@ export class DirectoryController {
                 })
                 return;
             };
+            const { ingredientId:ingredientIdStr } = req.params;
+            const ingredientId = parseInt(ingredientIdStr as string);
+            const ingredientUpdateData: IngredientUpdateType = req.body;
+            const ingredient = await DirectoryService.updateIngredient(ingredientId, ingredientUpdateData);
+            res.status(200).json(IngredientUpdateSchema.parse(ingredient));
         } catch (error: any) {
             res.status(400).json({
                 message: error.message,
@@ -162,6 +188,10 @@ export class DirectoryController {
                 })
                 return;
             };
+            const { ingredientId:ingredientIdStr } = req.params;
+            const ingredientId = parseInt(ingredientIdStr as string);
+            await DirectoryService.deleteIngredient(ingredientId);
+            res.status(204).send();
         } catch (error: any) {
             res.status(400).json({
                 message: error.message,
--- src/services/directory.service.ts
+++ src/services/directory.service.ts
@@ -1,5 +1,5 @@
 import { prisma } from "../config/database.js";
-import type { DishTypeCreateType, DishTypeUpdateType } from "../schemas/directory.schema.js";
+import type { DishTypeCreateType, DishTypeUpdateType, IngredientCreateType, IngredientUpdateType } from "../schemas/directory.schema.js";
 
 export class DirectoryService {
     static async getDishTypes(search: string, page: number, limit: number) {
@@ -69,6 +69,76 @@ export class DirectoryService {
         };
         await prisma.dishType.delete({
             where: { id: dishTypeId }
+        });
+    };
+
+    static async getIngredients(search: string, page: number, limit: number) {
+        const whereCondition: any = {};
+        if (search.trim().length > 0) {
+            whereCondition.title = {
+                contains: search.trim(),
+                mode: 'insensitive'
+            };
+        };
+        const skip = (page - 1) * limit;
+        const ingredients = await prisma.ingredient.findMany({
+            skip: skip,
+            take: limit,
+            where: whereCondition
+        });
+        return ingredients;
+    };
+
+    static async createIngredient(ingredientCreateData: IngredientCreateType) {
+        const { title } = ingredientCreateData;
+        const existingIngredient = await prisma.ingredient.findUnique({
+            where: { title },
+        });
+        if (existingIngredient) {
+            throw new Error('Тип блюда с таким названием уже существует');
+        };
+        const ingredient = await prisma.ingredient.create({
+            data: {
+                title
+            }
+        });
+        return ingredient;
+    };
+
+    static async getIngredient(ingredientId: number) {
+        const ingredient = await prisma.ingredient.findUnique({
+            where: { id: ingredientId }
+        });
+        if (!ingredient) {
+            throw new Error('Тип блюда не найден');
+        };
+        return ingredient;
+    };
+
+    static async updateIngredient(ingredientId: number, ingredientUpdateData: IngredientUpdateType) {
+        const ingredient = await prisma.ingredient.findUnique({
+            where: { id: ingredientId }
+        });
+        if (!ingredient) {
+            throw new Error('Тип блюда не найден');
+        };
+        const { title } = ingredientUpdateData;
+        const updatedIngredient = await prisma.ingredient.update({
+            where: { id: ingredientId },
+            data: { title }
+        })
+        return updatedIngredient;
+    };
+
+    static async deleteIngredient(ingredientId: number) {
+        const ingredient = await prisma.ingredient.findUnique({
+            where: { id: ingredientId }
+        });
+        if (!ingredient) {
+            throw new Error('Тип блюда не найден');
+        };
+        await prisma.ingredient.delete({
+            where: { id: ingredientId }
         });
     };
 }
