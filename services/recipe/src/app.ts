86e83e3ab1b9293c573ea499564bbea1a1791c81
vlad <vkm.blog@yandex.ru>
2026-06-08 17:24:49 +0300

    Commit

---
+++ services/recipe/src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import morgan from 'morgan';
+
+import routes from './routes';
+import { errorHandler } from './middleware/errorHandler';
+
+
+export const createApp = () => {
+    const app = express();
+
+    // app.use((req, _res, next) => {
+    //     console.log('Request Body:', req.body);
+    //     next();
+    // });
+
+    app.use(morgan('dev'));
+    app.use(express.json());
+    app.use('/api', routes);
+    app.use(errorHandler);
+    return app;
+}
