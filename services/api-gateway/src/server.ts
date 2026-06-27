8c586c99cac144cdf8ea5c251ee8adc6434e9bf7
vlad <vkm.blog@yandex.ru>
2026-06-27 16:46:22 +0300

    Commit

--- services/api-gateway/src/server.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from './app';
-import { config } from './config';
-
-const app = createApp();
-
-app.listen(config.port, () => {
-    console.log(`api-gateway запущен на порту ${config.port}`);
-});
